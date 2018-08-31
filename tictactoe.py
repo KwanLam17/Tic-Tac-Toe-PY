7c1807aea2cbf2ac6fe7b5279b20ff44702adc83
KwanLam <kwan.lam@alumni.ubc.ca>
2018-08-31 16:53:47 -0700

    def display_board/player_input f'ns

---
+++ tictactoe.py
@@ -0,0 +1,14 @@
+def display_board(board):
+    print(F" {board[1]} | {board[2]} | {board[3]}\n---|---|---\n {board[4]} | {board[5]} | {board[6]}\n---|---|---\n {board[7]} | {board[8]} | {board[9]}")
+
+def player_input():
+    marker = ''
+
+    while (marker != 'X') and (marker != 'O'):
+        marker = input("Please pick a marker 'X' or 'O': ")
+
+test_board = ['#','X','O','X','O','X','O','X','O','X']
+display_board(test_board)
+
+player1 = player_input()
+
